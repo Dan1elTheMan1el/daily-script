5dc328236640640e6411f4ee8f32db1216a23233
Daniel Friedman <danielfriedman@Daniels-MacBook-Air.local>
2025-12-06 20:25:02 -0800

    Fixed scripts import

--- daily.py
+++ daily.py
@@ -1,15 +1,21 @@
 from scripts import IPAupdates, crypticPuz
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+IPAwebhook = os.getenv("IPA_WEBHOOK")
+CROSSWORDwebhook = os.getenv("CROSSWORD_WEBHOOK")
 
 # Check for IPA Updates for my sideloaded apps
 try:
-    IPAupdates()
+    IPAupdates.IPAupdates(IPAwebhook)
     print("IPA updates check complete.")
 except Exception as e:
     print(f"Error checking IPA updates: {e}")
 
 # Scrape and convert latest Lovatts Cryptic Crossword
 try:
-    crypticPuz()
+    crypticPuz.crypticPuz(CROSSWORDwebhook)
     print("Cryptic crossword fetch complete.")
 except Exception as e:
     print(f"Error fetching cryptic crossword: {e}")
